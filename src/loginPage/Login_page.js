d1aa6f42c870c73b1a23c7e422eaca150d10b322
Mangtre503 <135637548+Mangtre503@users.noreply.github.com>
2024-04-21 15:48:03 +0700

    Update Login_page.js

--- src/loginPage/Login_page.js
+++ src/loginPage/Login_page.js
@@ -1,7 +1,7 @@
 import './Login_page.css';
 import { Fragment } from 'react';
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Route, Link, Routes } from "react-router-dom";
 
 const login_img=process.env.PUBLIC_URL + 'img/login_page.png'; 
 
@@ -9,6 +9,7 @@ function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [role, setRole] = useState('');
+
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
   };
@@ -21,6 +22,7 @@ function Login() {
   const login = () => {
     if (username.length > 0 && password.length > 0 && role.length >0) {
       alert(`Login successful! You are logged in as a ${role}.`);
+     
     } else {
       alert("Invalid username or password!");
     }
@@ -45,8 +47,25 @@ function Login() {
           <input type="text" id="username" onChange={handleUsernameChange} placeholder="Type your id or user name" />
           <input type="text" id="password" onChange={handlePasswordChange} placeholder="Type your password" />
           <div id="Buttons">
-            <button onClick={login}>Login</button>
-            <button>Change password</button>
+          {role === '' && (
+            
+              <button onClick={login}>Login</button>
+           
+          )}
+          {role === 'teacher' && (
+            <Link to="/Tea">
+              <button onClick={login}>Login</button>
+            </Link>
+          )}
+          {role === 'student' && (
+            <Link to="/Stu">
+              <button onClick={login}>Login</button>
+            </Link>
+          )}
+              
+              <Link id ="Goto" to="/ChangePw">
+                <button>Change password</button>
+              </Link>
           </div>               
       </div>
     </div>
