878786ebd4b95a2c68145abe7c3415a0d91b4bae
Mangtre503 <135637548+Mangtre503@users.noreply.github.com>
2024-04-24 05:33:53 +0700

    Update ChangePass.js

--- src/ChangePw/ChangePass.js
+++ src/ChangePw/ChangePass.js
@@ -1,10 +1,13 @@
 import './ChangePass.css';
 import React, { useState } from 'react';
+import {Link } from 'react-router-dom'
 const login_img=process.env.PUBLIC_URL + 'img/login_page.png'; 
 function ChangePassword(){
     const [username, setUsername]=useState('');
     const [old_password, setold_password]=useState('');
     const [new_password, setnew_password]=useState('');
+    const [role, setRole] = useState('');
+    const [IsChanged, setIsChanged] = useState(false);
     const checkValue = (event)=>{
         setUsername(event.target.value);
     }
@@ -14,9 +17,13 @@ function ChangePassword(){
     const updatePassword =(event)=>{
         setnew_password(event.target.value);
     }
+    const handleRoleChange = (event) => {
+        setRole(event.target.value);
+    };
     const change =()=>{
         if(username.length >0 && new_password.length>0 && old_password.length>0){
-            alert("Change password sussessful!");
+            alert("Change password sussessful! Please login.");
+            setIsChanged(true);
         } else {
             alert("Invalid username or password!");
         }
@@ -33,11 +40,21 @@ function ChangePassword(){
             </div>
             <div className="right_side">
                 <h2>Reset your password!</h2>
-                
+                <select className="role" onChange={handleRoleChange} >
+                    <option value="">Login as:</option>
+                    <option value="teacher">Teacher</option>
+                    <option value="student">Student</option>
+                </select>
                 <input type="text" id="username" onChange={checkValue} placeholder='User name' />
-                <input type="text" id="old_password" onChange={checkOldPassword} placeholder='Old password' />
-                <input type="text" id="new_password" onChange={updatePassword} placeholder='New password' />
+                <input type="password" id="old_password" onChange={checkOldPassword} placeholder='Old password' />
+                <input type="password" id="new_password" onChange={updatePassword} placeholder='New password' />
                 <button onClick={change}> Change password</button>
+                {IsChanged &&(
+                    <button>
+                         <Link to ='/'>Go to Login</Link>
+                    </button>         
+                    )
+                }
             </div>
             
         </div>
