d41c38d3e18494e968d7334dc8323d1322b1ec2a
zuyfarmm <phamduy11112004@gmail.com>
2024-04-30 22:38:18 +0700

    merge

--- src/model/Student.js
+++ src/model/Student.js
@@ -1,8 +1,10 @@
-import { db, ref, set, get, child, update, remove } from '../firebase/firebase';
+import { db, ref, set, get, update, remove } from '../firebase/firebase';
 import { Person } from './Person.js';
 import { getStuData } from '../firebase/firebasefunction';
 import { Score } from './Score.js';
 import { Account } from './Account.js';
+import { WeeklyFeedback } from './WeeklyFeedback.js';
+import { Feedback } from './Feedback.js';
 export class Student extends Person {
     #studentScores = new Map();
     #studentFeedback = new Map();
@@ -36,10 +38,23 @@ export class Student extends Person {
             const ScoreData = ref(db, `Student/${super.getID()}/Course/HK222/${courseID}`);
             const snapshot2 = await get(ScoreData);
             const getScore=snapshot2.val(); 
-            const scoreData = "KT: " + getScore.KT + " BTL: " + getScore.BTL + " TN:" + getScore.TN;
-            const ave= (parseFloat(getScore.KT) + parseFloat(getScore.BTL) + parseFloat(getScore.TN) + parseFloat(getScore.Final))/4;
-            const value = new Score(scoreData, getScore.Final, ave);
+            const value = new Score(getScore.componentScore, getScore.examScore, getScore.totalScore);
+            value.setIsAppeal(getScore.isAppeal);
+            value.setIsDone(getScore.isDone);
             this.#studentScores.set(courseID, value);
+            //Feedback
+            const getData=snapshot2.val(); 
+            const userRef2 = ref(db, `Course/${courseID}/Group/${getData.Class}/Student/${super.getID()}/Week`);
+            const snapshotData = await get(userRef2);
+            const arrayWeek = Object.keys(snapshotData.val() || {});
+            const feedback = new Feedback();
+            for(const week of arrayWeek){
+                const Data = ref(db, `Course/${courseID}/Group/${getData.Class}/Student/${super.getID()}/Week/${week}`);
+                const snapshot3 = await get(Data);
+                const getValue=snapshot3.val(); 
+                feedback.setFeedback(week, getValue.comment, getValue.score);
+            }
+            this.#studentFeedback.set(courseID, feedback);
         }
     }
 
@@ -58,18 +73,108 @@ export class Student extends Person {
         }
         return groups;
     }
-    async setStudentExamScore(courseName, scoreData){
+    async setScore(courseName, compoScore, finalScore, totalScore){
         for (let [course, score] of this.#studentScores.entries()) {
             if (course === courseName) {
                 const userScore = ref(db, `Student/${super.getID()}/Course/HK222/${courseName}`);
                 const snapshot = await get(userScore);
-                score.editScore(scoreData);
+                score.setAllScore(compoScore, finalScore, totalScore)
                 if(snapshot.exists()){
                     try {
                         await update(userScore, {
-                            Final: scoreData
+                            componentScore: compoScore,
+                            examScore: finalScore,
+                            totalScore: totalScore,
+                            isEdited: 1
                         });
-                        alert("User data updated successfully");
+                    } catch (error) {
+                        console.error("Error updating user data:", error);
+                    }
+                }
+                break;
+            }
+        }
+        return null;
+    }
+    async setExamScore(courseName, scoreData){
+        for (let [course, score] of this.#studentScores.entries()) {
+            if (course === courseName) {
+                const userScore = ref(db, `Student/${super.getID()}/Course/HK222/${courseName}`);
+                const snapshot = await get(userScore);
+                score.setFinalScore(scoreData);
+                if(snapshot.exists()){
+                    try {
+                        await update(userScore, {
+                            examScore: scoreData
+                        });
+                    } catch (error) {
+                        console.error("Error updating user data:", error);
+                    }
+                }
+                break;
+            }
+        }
+        return null;
+    }
+    async setTotalScore(courseName, scoreData){
+        for (let [course, score] of this.#studentScores.entries()) {
+            if (course === courseName) {
+                const userScore = ref(db, `Student/${super.getID()}/Course/HK222/${courseName}`);
+                const snapshot = await get(userScore);
+                score.setAverScore(scoreData);
+                score.setIsDone(true);
+                const data= snapshot.val();
+                if(snapshot.exists()){
+                    try {
+                        await update(userScore, {
+                            totalScore: scoreData
+                        });
+                        if(data.isAppeal===true) {
+                            await update(userScore, {
+                                isEdited: 2,
+                                isDone: true
+                            });
+                        }
+                    } catch (error) {
+                        console.error("Error updating user data:", error);
+                    }
+                }
+                break;
+            }
+        }
+        return null;
+    }
+    async setComponentScore(courseName, scoreData){
+        for (let [course, score] of this.#studentScores.entries()) {
+            if (course === courseName) {
+                const userScore = ref(db, `Student/${super.getID()}/Course/HK222/${courseName}`);
+                const snapshot = await get(userScore);
+                score.setComponentScore(scoreData);
+                if(snapshot.exists()){
+                    try {
+                        await update(userScore, {
+                            componentScore: scoreData
+                        });
+                    } catch (error) {
+                        console.error("Error updating user data:", error);
+                    }
+                }
+                break;
+            }
+        }
+        return null;
+    }
+    async setIsDone(courseName, isDone){
+        for (let [course, score] of this.#studentScores.entries()) {
+            if (course === courseName) {
+                const userScore = ref(db, `Student/${super.getID()}/Course/HK222/${courseName}`);
+                const snapshot = await get(userScore);
+                score.setIsDone(isDone);
+                if(snapshot.exists()){
+                    try {
+                        await update(userScore, {
+                            isDone: isDone
+                        });
                     } catch (error) {
                         console.error("Error updating user data:", error);
                     }
@@ -90,12 +195,75 @@ export class Student extends Person {
     getAllScore(){
         return this.#studentScores;
     }
-    setStudentFeedback(group, feedback){
-        this.#studentFeedback.set(group, feedback);
+    async setDay(courseName, startDay, endDay){
+        const userScore = ref(db, `Student/${super.getID()}/Course/HK222/${courseName}/appealTime`);
+        const snapshot = await get(userScore);
+        if(snapshot.exists()){
+            try {
+                await update(userScore, {
+                    EndTime: endDay,
+                    StartTime: startDay
+                });
+            } catch (error) {
+                console.error("Error updating user data:", error);
+            }
+        }
     }
-    getAGroupFeedback( groupName){
-        for (let [group, feedback] of this.#studentFeedback.entries()) {
-            if (group.getGroupName() === groupName) {
+    async setStudentFeedback(course, week, comment, rate){
+        const ScoreData = ref(db, `Student/${super.getID()}/Course/HK222/${course}`);
+        const snapshot2 = await get(ScoreData);
+        //Feedback
+        const getData=snapshot2.val(); 
+        const userRef2 = ref(db, `Course/${course}/Group/${getData.Class}/Student/${super.getID()}/Week/${week}`);
+        if (!this.#studentFeedback.has(course)) {
+            const newNodeData = {
+                comment: comment,
+                score: rate
+            };
+            await set(userRef2, newNodeData);
+            const feedback=new Feedback();
+            feedback.setFeedback(week, comment, rate);
+            this.#studentFeedback.set(course, feedback);
+        }else{
+            try {
+                await update(userRef2, {
+                    comment: comment,
+                    score: rate
+                });
+            } catch (error) {
+                console.error("Error updating user data:", error);
+            }
+            const courseFeedback = this.getAGroupFeedback(course).getAFeedback(week);
+            courseFeedback.setComment(comment);
+            courseFeedback.setRate(rate);
+        }
+    }
+    async addStudentFeedback(course, week, comment, rate){
+        const ScoreData = ref(db, `Student/${super.getID()}/Course/HK222/${course}`);
+        const snapshot2 = await get(ScoreData);
+        const getData=snapshot2.val(); 
+        const userRef2 = ref(db, `Course/${course}/Group/${getData.Class}/Student/${super.getID()}/Week/${week}`);
+            const newNodeData = {
+                comment: comment,
+                score: rate
+            };
+            await set(userRef2, newNodeData);
+            const courseFeedback = this.getAGroupFeedback(course);
+            courseFeedback.setFeedback(week, comment, rate);
+    }
+    async removeStudentFeedback(course, week){
+        const ScoreData = ref(db, `Student/${super.getID()}/Course/HK222/${course}`);
+        const snapshot2 = await get(ScoreData);
+        const getData=snapshot2.val(); 
+        const userRef2 = ref(db, `Course/${course}/Group/${getData.Class}/Student/${super.getID()}/Week/${week}`);
+        await remove(userRef2);
+
+        const courseFeedback = this.getAGroupFeedback(course);
+        courseFeedback.removeAFeedback(week);
+    }
+    getAGroupFeedback(courseName){
+        for (let [course, feedback] of this.#studentFeedback.entries()) {
+            if (course === courseName) {
                 return feedback;
             }
         }
--- src/pageTea/AppealAnnounce/AppealAnnounce.js
+++ src/pageTea/AppealAnnounce/AppealAnnounce.js
@@ -1,23 +1,53 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../../components/ComponentTea/HeaderTea/HeaderTea';
 import Footer from '../../components/ComponentTea/FooterTea/FooterTea';
 import Nav from '../../components/ComponentTea/NavTea/NavTea';
 import './AppealAnnounce.css';
-
+import { data } from '../../loginPage/Login_page';
 function AppealAnnounce() {
-    const [students, setStudents] = useState([
-        { name: 'Nguyễn Văn A', studentID: '001', course: 'Công nghệ phần mềm', courseID: 'CO2018', group: 'L01', appeal: false, isDone: false },
-        { name: 'Trần Thị B', studentID: '234567', course: 'Lập trình web', courseID: 'CS2001', group: 'L10', appeal: true, isDone: true },
-        { name: 'Phạm Văn C', studentID: '345678', course: 'Toán cao cấp', courseID: 'MT1005', group: 'L15', appeal: false, isDone: false },
-        { name: 'Nguyễn Văn D', studentID: '143456', course: 'Giải tích 1', courseID: 'MT1003', group: 'L07', appeal: false, isDone: true },
-        { name: 'Trần Thị E', studentID: '234367', course: 'Lập trình web', courseID: 'CS2001', group: 'L10', appeal: true, isDone: false },
-        { name: 'Phạm Văn F', studentID: '347378', course: 'Toán cao cấp', courseID: 'MT1005', group: 'L05', appeal: true, isDone: true },
-    ]);
+    const groups= data.getGroup();
+    const [students, setStudents] = useState([]);
+    useEffect(() => {
+        const loadGroup = async () => {
+          for (let groupData of groups) {
+            const arrStu = groupData.getStudents();
+            for (let StuData of arrStu) {
+                if(StuData.getStudentScore(groupData.getCourseID()).getIsAppeal()){
+                    setStudents((prevStudents) => {
+                        // Kiểm tra nếu sinh viên đã tồn tại
+                        const exists = prevStudents.some(
+                            (student) => student.studentID === StuData.getID() && student.courseID === groupData.getCourseID()
+                        );
+                        if (!exists) {
+                            return [
+                                ...prevStudents, // Giữ lại các phần tử hiện tại
+                                {
+                                    name: StuData.getName(),
+                                    studentID: StuData.getID(),
+                                    course: groupData.getCourseName(),
+                                    courseID: groupData.getCourseID(),
+                                    group: groupData.getName(),
+                                    isDone: StuData.getStudentScore(groupData.getCourseID()).getIsDone(),
+                                },
+                            ];
+                        }
+                        return prevStudents; // Trả về mảng hiện tại nếu đã tồn tại
+                    });
+                }
+            }
+          }
+        };
+        loadGroup();
+    }, [groups]);
+
 
     const handleCheckboxChange = (index) => {
         const updatedStudents = [...students];
         updatedStudents[index].isDone = !updatedStudents[index].isDone;
+        const groupData= data.getAGroup(updatedStudents[index].courseID, updatedStudents[index].group);
+        const StudentData = groupData.getAStudent(updatedStudents[index].studentID);
+        StudentData.setIsDone(groupData.getCourseID(),updatedStudents[index].isDone);
         setStudents(updatedStudents);
     };
 
